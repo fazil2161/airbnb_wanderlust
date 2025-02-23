7dc30044995c6ac25cd03cf2d9c99ca71b2c94b5
fazil2161 <fazilfpvazeer06@gmail.com>
2025-02-23 17:49:15 +0530

    dbUrl

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ const listingRouter= require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-dbUrl= "mongodb://127.0.0.1:27017/wanderlust"
-// const dbUrl = process.env.ATLASDB_URL;
+// dbUrl= "mongodb://127.0.0.1:27017/wanderlust"
+const dbUrl = process.env.ATLASDB_URL;
 
 main(). then(() => {
     console.log("connected to DB");
